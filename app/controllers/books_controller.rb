015b662612a27783136ecbd59b479bdc1e5cb68f
Parv Kumar <parvathikumar92@gmail.com>
2018-01-25 02:29:18 +0000

    added routes to books controller

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,21 @@
+class BooksController < ApplicationController
+
+
+  get '/books' do
+  end
+
+  get '/books/new' do
+  end
+
+  post '/books/new' do
+  end
+
+  get "/books/:id/edit" do
+  end
+
+  get '/books/edit' do
+  end
+
+  get '/books/delete' do
+  end
+end
