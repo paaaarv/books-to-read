1707dd100de351c863c9577eb0f6c7788c27bb48
Parv Kumar <parvathikumar92@gmail.com>
2018-01-31 03:06:11 +0000

    added helper methods to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,22 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+
+
+
+  helpers do
+
+    def logged_in?
+      if session[:user_id]
+        return self
+      else
+        return false
+      end
+    end
+
+    def current_user
+      @user = User.find(session[:user_id])
+      return @user
+    end
+  end
 end
